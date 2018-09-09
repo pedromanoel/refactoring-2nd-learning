41615a99243cdea391bab066b6fdb509178ad79a
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2018-09-08 23:36:17 -0300

    Change function declaration

--- src/statement.js
+++ src/statement.js
@@ -9,7 +9,7 @@ function statement (invoice, plays) {
       minimumFractionDigits: 2
     }).format
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
+    let thisAmount = amountFor(perf)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
@@ -24,10 +24,10 @@ function statement (invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`
   return result
 
-  function amountFor (aPerformance, play) {
+  function amountFor (aPerformance) {
     let result = 0
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case 'tragedy':
         result = 40000
         if (aPerformance.audience > 30) {
@@ -42,7 +42,7 @@ function statement (invoice, plays) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`unknown type: ${play.type}`)
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
     }
 
     return result
