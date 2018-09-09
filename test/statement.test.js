74d82f1e7d93346ed2186f34faa8381e78fc2d8c
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2018-09-09 02:08:25 -0300

    Add htmlStatement

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,6 +1,6 @@
 /* global test, expect */
 
-const statement = require('../src/statement')
+const { statement, htmlStatement } = require('../src/statement')
 const plays = require('./plays.json')
 const invoices = require('./invoices.json')
 
@@ -15,6 +15,25 @@ test('statement returns bill', () => {
     'You earned 47 credits',
     ''].join('\n'))
 })
+
+test('htmlStatement returns bill', () => {
+  const result = htmlStatement(invoices[0], plays)
+  expect(result).toBe([
+    '<h1>Statement for BigCo</h1>',
+    '<table>',
+    `  ${tr('th', 'play', 'seats', 'cost')}`,
+    `  ${tr('td', 'Hamlet', '55', '$650.00')}`,
+    `  ${tr('td', 'As You Like It', '35', '$580.00')}`,
+    `  ${tr('td', 'Othello', '40', '$500.00')}`,
+    '</table>',
+    '<p>Amount owed is <em>$1,730.00</em></p>',
+    '<p>You earned <em>47</em> credits</p>',
+    ''].join('\n'))
+})
+
+function tr (cellTag, ...cols) {
+  return `<tr>${cols.map(col => `<${cellTag}>${col}</${cellTag}>`).join('')}</tr>`
+}
 
 test('tragedy costs 400 USD', () => {
   const invoice = givenInvoiceWithPerformances({ playID: 'tragedy', audience: 0 })
