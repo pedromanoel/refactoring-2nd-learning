485790ca69ea4115749c1d8a6d81303f62a0ae4f
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2019-01-11 22:19:36 -0200

    Add standard-js to project

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,15 +1,17 @@
-const statement = require("../src/statement")
-const plays = require("./plays.json")
-const invoices = require("./invoices.json")
+/* global test, expect */
+
+const statement = require('../src/statement')
+const plays = require('./plays.json')
+const invoices = require('./invoices.json')
 
 test('statement returns bill', () => {
   const result = statement(invoices[0], plays)
   expect(result).toBe([
-    "Statement for BigCo",
-    "  Hamlet: $650.00 (55 seats)",
-    "  As You Like It: $580.00 (35 seats)",
-    "  Othello: $500.00 (40 seats)",
-    "Amount owed is $1,730.00",
-    "You earned 47 credits",
-    ""].join("\n"))
+    'Statement for BigCo',
+    '  Hamlet: $650.00 (55 seats)',
+    '  As You Like It: $580.00 (35 seats)',
+    '  Othello: $500.00 (40 seats)',
+    'Amount owed is $1,730.00',
+    'You earned 47 credits',
+    ''].join('\n'))
 })
