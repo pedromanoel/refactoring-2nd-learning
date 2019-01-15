2a3f9fbced9cb12f72469dff48b3f19b15e6c54a
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2019-01-14 22:13:37 -0200

    Move printDetails to outside scope

--- src/extract-method.js
+++ src/extract-method.js
@@ -14,19 +14,19 @@ module.exports = function withClock (Clock) {
       const today = Clock.today
       invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
 
-      printDetails()
+      printDetails(invoice, outstanding)
 
       function printBanner () {
         console.log('***********************')
         console.log('**** Customer Owes ****')
         console.log('***********************')
       }
-
-      function printDetails () {
-        console.log(`name: ${invoice.customer}`)
-        console.log(`amount: ${outstanding}`)
-        console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
-      }
     }
+  }
+
+  function printDetails (invoice, outstanding) {
+    console.log(`name: ${invoice.customer}`)
+    console.log(`amount: ${outstanding}`)
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
   }
 }
